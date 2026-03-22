ba243a6d33eccf43e576646a1e1d975d6c8ef596
Julia Persson <julia.persson@chasacademy.se>
2026-03-22 20:17:22 +0100

    Fix backup_attachment block in main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "google_compute_instance" "vm" {
 
   network_interface {
     network = "default"
-    access_config {} # Ger VM:en en extern IP
+    access_config {}
   }
 
   metadata_startup_script = file("startup.sh")
